e647b0a39d821661c801cb608f258022fc5535a2
alizaiter777 <alizaiter777@gmail.com>
2024-11-04 17:00:33 +0200

    ask user to enter values

---
+++ exc.py
@@ -0,0 +1,5 @@
+
+
+#using split function to split user input according to space
+#using map to convert each string to integer after splitting
+array = list(map(int, input("Enter list of integers: ").split()))
