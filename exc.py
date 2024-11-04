07bdb204946868baafdcb72a065ecf0c54ff59c0
alizaiter777 <alizaiter777@gmail.com>
2024-11-04 17:34:59 +0200

    create finds_peaks function

--- exc.py
+++ exc.py
@@ -6,3 +6,11 @@ array = list(map(int, input("Enter list of integers: ").split()))
 
 #generate peak list
 peaks = []
+
+#create peaks function that find the peaks value by comparing i with its neighbors
+# which i  we initialize it with index 1 
+def find_peaks(array):
+     for i in range(1, len(array) - 1):
+        if array[i] > array[i - 1] and array[i] > array[i + 1]:
+            peaks.append(array[i])
+     return peaks 
